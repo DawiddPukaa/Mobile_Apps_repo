6ed12f4e1bd1f4875e6f116d02ab9244d7e9913b
Dawid_Puka <dawidpuka@gmail.com>
2022-01-18 19:31:06 +0100

    finished Lab1

--- app/src/main/java/com/example/dawidpuka/CheckStudentAVG.java
+++
@@ -1,6 +0,0 @@
-package com.example.dawidpuka;
-
-public class CheckStudentAVG {
-
-
-}
--- app/src/main/java/com/example/dawidpuka/MainActivity.java
+++ app/src/main/java/com/example/dawidpuka/MainActivity.java
@@ -16,14 +16,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void check(View v) {
+        EditText grade = (EditText) findViewById(R.id.editTextAvergrade);
+        EditText name = (EditText) findViewById(R.id.editTextName);
+        EditText surName = (EditText) findViewById(R.id.editTextSurname);
+        TextView result = (TextView) findViewById(R.id.textViewResult2);
 
-        EditText grade = (EditText)findViewById(R.id.editTextAvergrade);
-        TextView result = (TextView)findViewById(R.id.textViewResult);
+        String nameS = String.valueOf(name.getText().toString());
+        String surNameS = String.valueOf(surName.getText().toString());
+        String resText = "Sudent: " + nameS + " " + surNameS;
+
         double avGrade = Double.valueOf(grade.getText().toString());
+
         if (avGrade < 4.5) {
-            result.setText("Scholarship: false :(");
+            result.setText(resText + " didn't get scholarship");
         } else {
-            result.setText("Scholarship: true! :)!");
+            result.setText(resText + " get scholarship");
         }
     }
+
 }
